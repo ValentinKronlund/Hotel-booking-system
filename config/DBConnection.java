fa655143584df3711d1c2bd7fc3f1bf5424c4d35
Valentin Sebastien <sebastien.kronlund@gmail.com>
2025-10-30 11:04:08 +0100

    Setup

---
+++ config/DBConnection.java
@@ -0,0 +1,9 @@
+package config;
+
+public class DBConnection {
+    private static final String Url = "jdbc:mysql://localhost:3306/HotelDB";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    
+}
